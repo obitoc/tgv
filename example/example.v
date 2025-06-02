55e2cc5fc610beb51b8b0cf6950e65cf4c083fba
Kira Edges <nekoedges@disroot.org>
2025-06-02 17:45:05 +0400

    Fixed example

--- example/example.v
+++ example/example.v
@@ -2,28 +2,28 @@ import tgv
 import x.json2
 // make bot
 
-bot := tgv.new("<BOT-TOKEN>")
+mut bot := tgv.new("<BOT-TOKEN>")
 
 // USE api for example
 
-bot.get_me()
+bot.get_me()!
 // etc ... see in ref
 
 // options map
 
-// example
-
 opts := {
 	"parse_mode":"html",
 }
+
 // btw you need to make id if int to string using id.str()
-bot.send_message(id,"msg",opts)
+bot.send_message(id,"msg",opts)!
 // empty options
 
-bot.send_message(id,"msg",map[string]string{})
-
-// btw if something went wrong see compiler output maybe you need to add or | !
+bot.send_message(id,"msg",map[string]string{})!
 
 // for bot message Context
 bot.on(fn [bot](ctx json2.Any,typeo string){})
 // pass a func to handler pass bot into the scope 2 params one is Context and two is type of context
+
+
+// TODO: make it work `v new` way
